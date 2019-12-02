667123965b3ffc8f985f151fa9ecf71b4286a7a6
will8889 <57931596+will8889@users.noreply.github.com>
2019-12-02 09:07:46 +0700

    Update no12.py

--- no12.py
+++ no12.py
@@ -1,13 +1,13 @@
 import string
-def caeser_cipher(text:str):
+def caeser_cipher(text:str,n):
     alphabet = string.ascii_lowercase
     new_word = ""
     for x in text:
-        next = alphabet.find(x) + 13
-        if next > 26:
-            next = next-26
+        next = alphabet.find(x) + n
+        if next > 25:
+            next = next-25
         new_letter = alphabet[next]
         new_word = new_word +new_letter
     return new_word
 
-print(caeser_cipher('hello'))
+print(caeser_cipher('hello',13))
